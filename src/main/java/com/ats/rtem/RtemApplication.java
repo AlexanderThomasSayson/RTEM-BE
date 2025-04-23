0bc2e92022e93a5946b25e5ef753db5ac2143abf
Alex <alex.srbootcamp@gmail.com>
2025-04-23 13:42:41 +0800

    initial commit

---
+++ src/main/java/com/ats/rtem/RtemApplication.java
@@ -0,0 +1,13 @@
+package com.ats.rtem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ats/rtem/RtemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.rtem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
