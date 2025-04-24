83f102bf9fc47e7fde66ac22b695d5dae84100d8
Alex <alex.srbootcamp@gmail.com>
2025-04-24 15:49:24 +0800

    feat: add finding of username or email.

--- src/main/java/com/ats/rtem/domain/dao/UserDao.java
+++ src/main/java/com/ats/rtem/domain/dao/UserDao.java
@@ -4,6 +4,14 @@ import com.ats.rtem.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserNameOrEmail(String userName, String email);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByUserName(String userName);
 }
