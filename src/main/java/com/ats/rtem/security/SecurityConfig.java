83ec42b2f20aae35c938735616c3fc4f47003d1d
Alex <alex.srbootcamp@gmail.com>
2025-04-25 17:43:19 +0800

    chore: temporarily allows endpoints publicly.

--- src/main/java/com/ats/rtem/security/SecurityConfig.java
+++ src/main/java/com/ats/rtem/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
             "/webjars/**",
             "/api/v1/auth/login",
             "/api/v1/auth/register",
+            "/api/v1/**"
     };
 
     // User-Only Endpoints
