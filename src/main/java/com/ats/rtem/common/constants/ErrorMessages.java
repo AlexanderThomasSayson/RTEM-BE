56031c8aa5852037d2148ccd80d7c9d226700fd0
Alex <alex.srbootcamp@gmail.com>
2025-04-25 17:44:29 +0800

    feat: add role error messages.

--- src/main/java/com/ats/rtem/common/constants/ErrorMessages.java
+++ src/main/java/com/ats/rtem/common/constants/ErrorMessages.java
@@ -2,4 +2,5 @@ package com.ats.rtem.common.constants;
 
 public class ErrorMessages {
     public static final String USER_NOT_FOUND = "User with an ID: %d does not exist!";
+    public static final String ROLE_NOT_FOUND = "Role with an ID: %d does not exist!";
 }
